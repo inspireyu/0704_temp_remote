eda19cfe33a34ac70d9f6705f3e03e0421ef5f81
YuChenrui <2258971941@qq.com>
2026-08-10 20:54:02 +0800

    TiJiaoXinXi-2

---
+++ gmx-learning_01.py
@@ -0,0 +1,98 @@
+
+"""
+number1 = 100 + 1
+number2 = 100 * 2
+number3 = 100 / 2
+number4 = 100 ** 2
+number5 = 100 % 3
+number6 = 3 * (4 + 5)
+a = "hello"
+b = True
+b = False
+"""
+
+"""
+a = input("number:")
+a_int = int(a)
+a_str = str(a_int)
+b = a_int ** 2
+print(f"Number is:{b}")
+# print(b)
+"""
+
+"""
+a = 10
+b = 2
+
+c = a # c=10
+a = b # a=2
+b = c # b=10
+"""
+
+"""
+ss = "hello"
+bb = False
+cc = 100 / 2
+
+t_a = type(a)
+t_ss = type(ss)
+t_bb = type(bb)
+t_cc = type(cc)
+print(t_ss)
+print(t_bb)
+print(t_cc)
+print(cc)
+"""
+
+"""
+a = 1
+b = isinstance(a, int)
+print(b)
+"""
+
+'''
+a = """hello
+
+
+hinao"""
+b = 100
+
+c = isinstance(a,type(b))
+print(a)
+'''
+
+"""
+a = 2
+a = a / 3
+a = a * 3
+print(a)
+"""
+
+
+# 转义字符
+"""
+print("he\\\'llo\nnaff\tme file")
+"""
+
+"""
+print("aaa", end = "")
+a = 1
+print(a)
+"""
+
+# 互换
+# type
+# instance(num, type)
+# 两个类型是否一致
+
+# 多行字符串
+# 转义字符 \' \" \n \t
+# end = ""
+
+s = []
+print(s)
+
+a = "hello"
+print(a[1])
+
+print("hello", end = "")
